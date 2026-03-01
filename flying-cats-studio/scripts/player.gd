d87830882c7fdecbf89a3f05986692cd05fa80b9
isobellz101 <isyu@s.sfusd.edu>
2026-03-01 01:11:41 -0800

    added the progressbar & code to try and make it work (ERROR)

--- flying-cats-studio/scripts/player.gd
+++ flying-cats-studio/scripts/player.gd
@@ -1,8 +1,61 @@
 extends CharacterBody2D
 
+class_name Player
+
+signal healthChanged
+
+@export var speed: int = 35
+@onready var animations = $AnimationPlayer
+@onready var effects: Node2D = $Effects
 @export var max_speed := 800.0
 @export var acceleration := 1200.0
 @export var deceleration := 1100.0
+@onready var hurt_box: Area2D = $HurtBox
+@onready var hurt_timer: Timer = $HurtTimer
+@onready var weapon: Sprite2D = $Weapon
+
+@export var maxHealth = 30
+@onready var currentHealth: int = maxHealth
+
+@export var knockbackPower: int = 500
+
+var lastAnimDirection: String = "Down"
+var isHurt: bool = false
+var isAttacking: bool = false
+
+func _ready():
+	effects.play("RESET")
+	
+func handleInput():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection*speed
+	
+	if Input.is_action_just_pressed("attack"):
+		attack()
+		
+func attack():
+	animations.play("attack" + lastAnimDirection)
+	isAttacking = true
+	weapon.enable()
+	await animations.animation_finished
+	weapon.disable()
+	isAttacking = false		
+			
+func updateAnimation():
+	if isAttacking: return
+	
+	if velocity.length() == 0:
+		if animations.is_playing():
+			animations.stop()
+			
+	else:
+		var direction = "Down"
+		if velocity.x < 0: direction = "Left"
+		elif velocity.x > 0: direction = "Right"
+		elif velocity.y < 0: direction = "Up"
+		
+		animations.play("walk" + direction)
+		lastAnimDirection = direction
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -13,3 +66,31 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 	move_and_slide()
+	handleInput()
+	updateAnimation()
+	if !isHurt:
+		for area in hurt_box.get_overlapping_areas():
+			if area.name == "hitBox":
+				hurtByEnemy(area)
+
+func hurtByEnemy(area):
+	currentHealth -= 10
+	if currentHealth < 0:
+		currentHealth = maxHealth
+		
+	isHurt = true
+	healthChanged.emit()
+	
+	knockback(area.get_parent().velocity)
+	effects.play("hurtBlink")
+	hurt_timer.start()
+	await hurt_timer.timeout
+	effects.play("RESET")
+	isHurt = false
+	
+func knockback(enemyVelocity: Vector2):
+	var knockbackDirection = (enemyVelocity - velocity).normalized() * knockbackPower
+	velocity = knockbackDirection
+	move_and_slide()
+	
+	
