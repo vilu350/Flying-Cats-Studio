2f7fc5c4a362dd95b379afc62eea05dbc1c4370d
isobellz101 <isyu@s.sfusd.edu>
2026-03-06 15:19:26 -0800

    Completed the healthbar and it works!

--- flying-cats-studio/scripts/player.gd
+++ flying-cats-studio/scripts/player.gd
@@ -4,7 +4,7 @@ class_name Player
 
 signal healthChanged
 
-@export var speed: int = 35
+@export var speed: int = 500
 @onready var animations = $AnimationPlayer
 @onready var effects: Node2D = $Effects
 @export var max_speed := 800.0
@@ -28,8 +28,7 @@ func _ready():
 	pass
 	
 func handleInput():
-	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = moveDirection*speed
+	pass
 	
 	#if Input.is_action_just_pressed("attack"):
 		#attack()
@@ -42,21 +41,21 @@ func handleInput():
 	#weapon.disable()
 	#isAttacking = false		
 			
-func updateAnimation():
+#func updateAnimation():
 	#if isAttacking: return
 	
-	if velocity.length() == 0:
-		if animations.is_playing():
-			animations.stop()
+	#if velocity.length() == 0:
+		#if animations.is_playing():
+			#animations.stop()
 			
-	else:
-		var direction = "Down"
-		if velocity.x < 0: direction = "Left"
-		elif velocity.x > 0: direction = "Right"
-		elif velocity.y < 0: direction = "Up"
+	#else:
+		#var direction = "Down"
+		#if velocity.x < 0: direction = "Left"
+		#elif velocity.x > 0: direction = "Right"
+		#elif velocity.y < 0: direction = "Up"
 		
-		animations.play("walk" + direction)
-		lastAnimDirection = direction
+		#animations.play("walk" + direction)
+		#lastAnimDirection = direction
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -68,10 +67,10 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 	move_and_slide()
 	handleInput()
-	updateAnimation()
+	#updateAnimation()
 	if !isHurt:
 		for area in hurt_box.get_overlapping_areas():
-			if area.name == "hitBox":
+			if area.name == "HitBox":
 				hurtByEnemy(area)
 
 func hurtByEnemy(area):
@@ -83,10 +82,10 @@ func hurtByEnemy(area):
 	healthChanged.emit()
 	
 	knockback(area.get_parent().velocity)
-	effects.play("hurtBlink")
+	#effects.play("hurtBlink")
 	hurt_timer.start()
 	await hurt_timer.timeout
-	effects.play("RESET")
+	#effects.play("RESET")
 	isHurt = false
 	
 func knockback(enemyVelocity: Vector2):
